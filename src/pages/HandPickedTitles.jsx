d26cbf8769ff7864fb4a8601f9ae545b16cbe540
adarshswaminath <adarshswaminath7@gmail.com>
2024-08-21 15:29:29 +0530

    curev added

--- src/pages/HandPickedTitles.jsx
+++ src/pages/HandPickedTitles.jsx
@@ -1,39 +1,51 @@
-
+import aboutbar from "../assets/about_bar1.png";
 
 function HandPickedTitles() {
   return (
-    <div className="flex items-center flex-wrap justify-center gap-3">
-      {Array.from({length:3}).map((_,index) => (
-        <div className="w-96 mx-auto">
-          <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
-            <div className="w-full aspect-w-16 aspect-h-10 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden xl:aspect-w-16 xl:aspect-h-10 relative">
-              <img
-                src={blogContent.image}
-                alt="thumbnail"
-                layout="fill"
-                objectFit="cover"
-                className="group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200"
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="font-bold my-4 text-lg text-zinc-700">
-                {blogContent.title}
-              </h2>
-              <h2 className="font-normal my-4 text-sm text-zinc-500">
-                {blogContent.description}
-              </h2>
-              <div className="flex flex-row justify-between items-center mt-10 w-full gap-3">
-              <div className="text-center relative z-10 px-6 py-2 bg-white text-black border border-black font-bold rounded-xl block text-xs w-full">
-                  contact
-                </div>
-                <div className="texet-center relative z-10 px-6 py-2 bg-black text-white font-bold rounded-xl block text-xs w-full">
-                  Read More
+    <div className="bg-white min-h-screen">
+      <div className="w-full flex justify-start bg-white">
+        <img src={aboutbar} className="w-fit h-8 md:h-16 rotate-180 " alt="" />
+      </div>
+      <div className="flex flex-col items-center mt-12">
+        <div className="flex flex-col items-end md:items-end">
+          <span className="font-light text-2xl lg:text-6xl text-center">
+            Program Open for Enrollment
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center flex-wrap justify-center gap-3 mt-12">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div className="w-96 mx-auto">
+            <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
+              <div className="w-full aspect-w-16 aspect-h-10 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden xl:aspect-w-16 xl:aspect-h-10 relative">
+                <img
+                  src={blogContent.image}
+                  alt="thumbnail"
+                  layout="fill"
+                  objectFit="cover"
+                  className="group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="font-bold my-4 text-lg text-zinc-700">
+                  {blogContent.title}
+                </h2>
+                <h2 className="font-normal my-4 text-sm text-zinc-500">
+                  {blogContent.description}
+                </h2>
+                <div className="flex flex-row justify-between items-center mt-10 w-full gap-3">
+                  <div className="text-center relative z-10 px-6 py-2 bg-white text-black border border-black font-bold rounded-xl block text-xs w-full">
+                    contact
+                  </div>
+                  <div className="texet-center relative z-10 px-6 py-2 bg-black text-white font-bold rounded-xl block text-xs w-full">
+                    Read More
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+        ))}
       </div>
-      ))}
     </div>
   );
 }
@@ -42,9 +54,8 @@ const blogContent = {
   title: "Amazing Tailwindcss Grid Layout Examples",
   description:
     "Grids are cool, but Tailwindcss grids are cooler. In this article, we will learn how to create amazing Grid layouts with Tailwindcs grid and React.",
-  image: "https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp",
+  image:
+    "https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp",
 };
 
-
-
-export default  HandPickedTitles
+export default HandPickedTitles;
