143d06892e953be8115d4fb3bdf26a61fd8e73ca
adarshswaminath <adarshswaminath7@gmail.com>
2024-08-25 11:32:17 +0530

    feat: navlink image redirect to roote added for the logo

--- src/landingpage/Navigation.jsx
+++ src/landingpage/Navigation.jsx
@@ -18,11 +18,11 @@ const Navigation = () => {
     <nav
       className={`flex justify-between fixed z-50 bg-[#FEFEFE] items-center w-full py-3 md:py-2.5 px-3 md:px-12 font-archivo border-b-2`}
     >
-      <img
+      <Link to="/"><img
         src={logo1}
         alt=""
         className="object-cover w-56"
-      />
+      /></Link>
       <ul className="md:flex gap-8 items-center hidden">
         {pages.map((page, index) => (
           <li key={index}>
