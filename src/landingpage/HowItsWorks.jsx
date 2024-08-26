c3a065bef1a1396b42c7da235432ff177f6d2c43
adarshswaminath <adarshswaminath7@gmail.com>
2024-08-26 14:33:58 +0530

    fix: added svg for the how its work and implement some styling

--- src/landingpage/HowItsWorks.jsx
+++ src/landingpage/HowItsWorks.jsx
@@ -172,7 +172,32 @@ export default function HowItsWorks() {
   return (
     <div id="howitswork" className="flex flex-col">
       {/* SVG Backgrounds */}
-      
+      <svg
+        width="1280"
+        height="197"
+        viewBox="0 0 1280 197"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-full h-full hidden md:block translate-y-[8.7rem]"
+      >
+        <path
+          d="M-30 90C-30 76.7452 -19.2548 66 -6 66H640H938.473C946.322 66 953.675 62.1622 958.163 55.723L989.837 10.277C994.325 3.83784 1001.68 0 1009.53 0L1286 0C1299.25 0 1310 10.7452 1310 24V173C1310 186.255 1299.25 197 1286 197H-6.00004C-19.2549 197 -30 186.255 -30 173V90Z"
+          fill="#010101"
+        />
+      </svg>
+      <svg
+        width="390"
+        height="228"
+        viewBox="0 0 390 228"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-full h-full block md:hidden translate-y-[11.5rem]"
+      >
+        <path
+          d="M-15 47.4823C-15 37.5411 -6.94112 29.4822 3 29.4822H115H184.13C190.015 29.4822 195.528 26.6059 198.894 21.7794L208.712 7.70285C212.078 2.87633 217.591 0 223.476 0H386C395.941 0 404 8.05887 404 18V210C404 219.941 395.941 228 386 228H3.00001C-6.94112 228 -15 219.941 -15 210V47.4823Z"
+          fill="#010101"
+        />
+      </svg>
       <div className="bg-black font-archivo flex flex-col z-10 items-center min-h-screen">
       <div className="flex items-center justify-center mt-4">
         <Button className="flex justify-around items-center gap-2 font-archivo text-sm md:text-lg text-[#01010199] bg-[#F7F7F7] border border-[#01010108] rounded-full tracking-wider px-4 py-2">
@@ -182,10 +207,10 @@ export default function HowItsWorks() {
       </div>
         <div className="flex flex-col items-center gap-3 md:gap-6 lg:my-12 p-2 lg:mx-16 text-center">
           <div className="text-center flex flex-col justify-center mx-auto items-center gap-4 top-0 lg:sticky z-50 bg-black mb-14 rounded-2xl">
-            <span className="text-white font-medium tracking-wide text-2xl md:text-5xl">
+            <span className="text-white tracking-wide text-2xl md:text-5xl font-light">
               How Knowlumi Revolutionizes Engineering Education!
             </span>
-            <span className="font-medium text-center text-white">
+            <span className="font-medium text-center text-[#88DB1B]">
               Build Your Dream Career with Our AI-Powered Parallel Education
               System
             </span>
