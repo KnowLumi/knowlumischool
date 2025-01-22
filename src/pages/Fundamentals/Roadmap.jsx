7ef75cd1c9cdfe8c0dc210a9f221125eb6af1cc5
adarshswaminath <adarshswaminath7@gmail.com>
2025-01-22 22:24:47 +0530

    fix: issue #2

--- src/pages/Fundamentals/Roadmap.jsx
+++ src/pages/Fundamentals/Roadmap.jsx
@@ -18,7 +18,8 @@ const Roadmap = (props) => {
   const {tracks,  id } = props;
   console.log(tracks[id]?.roadmap[0].heading)
   return (
-    <div className="flex flex-col gap-12 py-6 md:py-9 px-4 md:px-12 mx-6 md:mx-20 bg-white shadow-2xl rounded-3xl mb-12">
+<>
+{tracks &&     <div className="flex flex-col gap-12 py-6 md:py-9 px-4 md:px-12 mx-6 md:mx-20 bg-white shadow-2xl rounded-3xl mb-12">
       <div className="flex gap-4 items-center">
         <i className="flex justify-center items-center w-11 h-11 bg-[#F3FEE6] rounded-full p-2.5">
           <IoNavigateCircleOutline className="-rotate-45 w-5 h-5" />
@@ -92,7 +93,8 @@ const Roadmap = (props) => {
           </span>
         </div>
       </div>
-    </div>
+    </div>}
+</>
   );
 };
 
